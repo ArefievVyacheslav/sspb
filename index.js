53b8fe8897610af59e66a0229fbed1424516848e
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-01-03 15:41:23 +0300

    роуты для получения данных в дашборд

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
 const fs = require('fs')
 const path = require('path')
+const getStatus = require('./utils/getStatus')
+
 
 server.use(bodyParser.json(), cors())
+
 
 server.post('/login', (req, res) => {
   const token = jwt.sign({ user: 'user name', admin: true }, 'ssp', { expiresIn: '30d' })
@@ -16,16 +19,18 @@ server.post('/login', (req, res) => {
     res.status(400).send(e)
   }
 })
-
 server.get('/proxy', (req, res) => {
   const proxies = fs.readFileSync(path.join(__dirname, './proxies.txt'), 'utf-8')
   res.send(proxies.split('\n').filter(proxy => !!proxy))
 })
-
 server.put('/proxy', (req, res) => {
   const proxies = req.body.reduce((acc, proxy) => acc += proxy + '\n', '')
   fs.writeFileSync(path.join(__dirname, './proxies.txt'), proxies)
   res.status(200).send('Прокси успешно записаны!')
 })
+server.get('/status', async (req, res) => {
+  res.status(200).send(await getStatus())
+})
+
 
 server.listen(3002, () => console.log('SERVER ON 3002'))
