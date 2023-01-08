10aec42187aaf69676ccbcf656c65bb2f64db8fb
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-01-08 09:04:48 +0300

    роут на матрицу товаров текущих с новыми

--- utils/startParsing.js
+++ utils/startParsing.js
@@ -3,8 +3,8 @@ const { PythonShell } = require('python-shell')
 const path = require('path')
 
 
-module.exports = params => {
-  if (params.all) startAllParsers()
+module.exports = async params => {
+  if (params.all) await startAllParsers()
   else PythonShell.run(path.join(__dirname, `../../sspp/advcake/${params.shop}/START.py`), null, function (err) {
     if (err) throw err
   })
